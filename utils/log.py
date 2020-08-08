62f91c999e1c6c4a3fe18f8348b1cbf165951b43
Wilker <Wilker.rj@gmail.com>
2020-08-08 20:18:50 -0300

    Adding logger

---
+++ utils/log.py
@@ -0,0 +1,20 @@
+import logging
+import colorlog
+
+def init_logger(dunder_name, testing_mode) -> logging.Logger:
+    log_format = (
+        '[%(asctime)s]'
+        '[%(name)s]'
+        '[%(funcName)s]'
+        '[%(levelname)s] - '
+        '%(message)s'
+    )
+    bold_seq = '\033[1m'
+    colorlog_format = (
+        '%(log_color)s '
+        f'{log_format}'
+    )
+    logging.basicConfig(level=logging.DEBUG)
+    colorlog.basicConfig(format=colorlog_format)
+    logger = logging.getLogger(dunder_name)
+    return logger
